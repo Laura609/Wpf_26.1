c7a2182cf106d3809ac294ee8299459aa0cbfbb6
Laura <lavrusik515@gmail.com>
2023-12-27 23:27:10 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_26._1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //if (System.IO.File.Exists("file.txt"))
+            //{
+            //    string xaml = System.IO.File.ReadAllText("file.txt");
+            //    object content = System.Windows.Markup.XamlReader.Parse(xaml);
+            //    this.Content = content;
+            //}
+        }
+
+        //protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        //{
+        //    base.OnClosing(e);
+        //    string xaml = System.Windows.Markup.XamlWriter.Save(this.Content);
+        //    System.IO.File.WriteAllText("file.txt", xaml);
+        //}
+    }
+}
